ae29c13129a3753fa653ded69cc8f5c1753122f3
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-11 08:15:43 -0300

    #

--- react-native/ReactNativeComponents/App.js
+++ react-native/ReactNativeComponents/App.js
@@ -1,19 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import KAV from './components/KAV';
+import SpinnerComponent from './components/SpinnerComponent';
 
 export default function App() {
   return (
-    <KAV />
+    <SpinnerComponent />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
