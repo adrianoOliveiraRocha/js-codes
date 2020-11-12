0e7ca6d8e2b8adf942b48e0fb94678775a36de74
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-12 07:24:35 -0300

    #

--- react-native/ReactNativeComponents/App.js
+++ react-native/ReactNativeComponents/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SpinnerComponent from './components/SpinnerComponent';
+import CameraTest from './components/CameraTest';
 
 export default function App() {
   return (
-    <SpinnerComponent />
+    <CameraTest />
   );
 }
