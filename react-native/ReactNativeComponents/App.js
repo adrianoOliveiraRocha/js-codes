f73355cb1e56c8aeb0f2f80e92867264b2a8b067
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-13 08:19:56 -0300

    picker select example

--- react-native/ReactNativeComponents/App.js
+++ react-native/ReactNativeComponents/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CameraTest from './components/CameraTest';
+import SelectTest from './components/SelectTest';
 
 export default function App() {
   return (
-    <CameraTest />
+    <SelectTest />
   );
 }
