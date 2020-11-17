a3f77ec6e916c576badec6aa945e9f04f9833314
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-17 08:05:33 -0300

    #

--- react-native/ReactNativeComponents/App.js
+++ react-native/ReactNativeComponents/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import BlurViewTest from './components/BlurViewTest';
+import CellularInfoTest from './components/CellularInfoTest';
 
 export default function App() {
   return (
-    <BlurViewTest />
+    <CellularInfoTest />
   );
 }
