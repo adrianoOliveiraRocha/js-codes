c2633c1bd1799dbda2d2739976cb22047369f6ca
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-23 15:41:43 -0300

    #

--- masteringJSFunctionalProgramming/keep/memoize3.js
+++ masteringJSFunctionalProgramming/keep/memoize3.js
@@ -1,12 +1,14 @@
-const memoize3 = fn => {
-    let cache = {}
-    const PRIMITIVES = ["number", "string", "boolean"]
-    return (...args) => {
-        let strX = args.length === 1 && PRIMITIVES.includes(typeof args[0]) ?
-            args[0] :
-            JSON.stringify(args)
-        return strX in cache ? cache[strX] : (cache[strX] = fn(...args))
-    }
+function memoize3(fn) {
+  let cache = {};
+  const PRIMITIVES = ["number", "string", "boolean"];
+  return function(...args) {
+    let strX = args.length === 1 && PRIMITIVES.includes(typeof args[0])
+      ? args[0]
+      : JSON.stringify(args);
+    return strX in cache
+      ? cache[strX]
+      : (cache[strX] = fn(...args));
+  }
 }
 
 module.exports = memoize3
--- masteringJSFunctionalProgramming/test.js
+++ masteringJSFunctionalProgramming/test.js
@@ -1,32 +1,12 @@
-function myPut(text, name, tStart, tEnd) {
-  console.log(`${name} - ${text} - ${tEnd} - ${tStart} = ${tEnd - tStart} ms`);
-}
-
-function myGet() {
-  return Date.now();
-}
-
-function addTiming(fn, getTime = myGet, output = myPut) {
-  return function(...args) {
-    let tStart = getTime();
-    try {
-      const valueToReturn = fn(...args);
-      output("normal exit", fn.name, tStart, getTime());
-      return valueToReturn;
-    } catch (e) {
-      output("exception throw", fn.name, tStart, getTime());
-      throw e;
-    }
+function fib(n) {
+  if(n == 0) {
+    return 0
+  } else if(n == 1) {
+    return 1;
+  } else {
+    console.log(`${n - 2} + ${n - 1} = ${(n -2) + (n - 1)}`);
+    return fib(n - 1) + fib(n - 2);
   }
 }
 
-function mult(a, arr) {
-  let result = 0;
-  arr.forEach((item, i) => {
-    result += item * i;
-  });
-  return result
-}
-
-const result = addTiming(mult)(778, [7, 8, 9, 9, 5, 5, 5, 34, 556, 3332]);
-console.log(result);
+console.log(fib(5));
