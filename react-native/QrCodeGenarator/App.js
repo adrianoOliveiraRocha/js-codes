8362729e317f9b13f3a4998ba0ef215b600ea9c9
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-12-03 13:42:38 -0300

    #

--- react-native/QrCodeGenarator/App.js
+++ react-native/QrCodeGenarator/App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import SvgQRCode from 'react-native-qrcode-svg';
 
+// https://docs.expo.io/tutorial/sharing/
 
 export default function App() {
+
+  const [shareCode, setShareCode] = React.useState(null);
+
+  var img;
+  function Simple() {
+    // setShareCode(img);
+    img = <SvgQRCode value="https://w3software.net.br"/>;
+    return img;
+  }
+
+  function share() {
+    setShareCode(img);
+    console.log(shareCode);
+    alert(img);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Simple />
+      <Button title="Share" onPress={share}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +34,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-around',
+    paddingTop: 20,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
 });
