a9befc570e0ed10b8ea4acd22935155a60f1d8e9
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-12-21 09:02:18 -0300

    #

--- algorithms/main.js
+++ algorithms/main.js
@@ -1,32 +1,50 @@
 'use strict';
+const res = {}
 
 function waysAcordingThefactor(factor, n) {
+	// console.log(factor, n);
 	let ways=[]
-	// let keep = []
+	
 	for(let i=1; i<n; i++) {
 		let way = new Array(i);
 		way.fill(factor, 0, i)				
-		// ways.push(way);
+		
 		let sum = way.reduce((accum, current) => accum + current);
 				
 		if(sum <= n) {
 			while(sum < n) {
 				sum+=1;
 				way.push(1);
-				// console.log(way)
 			}
 		}
 		
-		if(sum == n) ways.push(way)		
+		// console.log(way)
+		if(sum == n) ways.push(way);		
+		else if(sum > 5) break;
 
 	}
 	
-	let next = ways[0].slice(1, -1);
-	console.log("\n next");
-	console.log(next);
-
-	return ways;
+	console.log(ways.join("\n"));
+	res[factor] = ways;
+	
+	let newn = n - factor;
+	let newfactor = factor - 1;
+	if(newn > 1 && newfactor > 1 ) {
+		res[factor] = waysAcordingThefactor(newfactor, newn);
+		console.log(factor.toString() + ",")
+		waysAcordingThefactor(newfactor, newn);
+	}
 
 }
 
-console.log(waysAcordingThefactor(4, 12))
+
+waysAcordingThefactor(5, 5);
+waysAcordingThefactor(4, 5);
+waysAcordingThefactor(3, 5);
+waysAcordingThefactor(2, 5);
+waysAcordingThefactor(1, 5);
+
+console.log(res)
+
+
+
