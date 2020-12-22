b32196d7a724991989df197f0635f40eeed9b57e
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-12-22 08:21:34 -0300

    #

--- algorithms/main.js
+++ algorithms/main.js
@@ -1,50 +1,60 @@
 'use strict';
-const res = {}
 
-function waysAcordingThefactor(factor, n) {
-	// console.log(factor, n);
+function waysAcordingThefactor(factor, n, res={}) {
+	
 	let ways=[]
-	
-	for(let i=1; i<n; i++) {
-		let way = new Array(i);
-		way.fill(factor, 0, i)				
-		
-		let sum = way.reduce((accum, current) => accum + current);
-				
-		if(sum <= n) {
-			while(sum < n) {
-				sum+=1;
-				way.push(1);
-			}
-		}
-		
-		// console.log(way)
-		if(sum == n) ways.push(way);		
-		else if(sum > 5) break;
+	if(factor === 1) {
+		let way = new Array(n);
+		way.fill(1, 0, (n + 1));
+		ways.push(way);
+	} else {
 
+		for(let i=1; i<n; i++) {
+			let way = new Array(i);
+			way.fill(factor, 0, i);				
+			
+			let sum = way.reduce((accum, current) => accum + current);
+					
+			if(sum <= n) {
+				while(sum < n) {
+					sum+=1;
+					way.push(1);
+				}
+			}
+			
+			if(sum == n) ways.push(way);		
+			else if(sum > n) break;
+
+		}
 	}
-	
-	console.log(ways.join("\n"));
+		
 	res[factor] = ways;
 	
 	let newn = n - factor;
 	let newfactor = factor - 1;
 	if(newn > 1 && newfactor > 1 ) {
-		res[factor] = waysAcordingThefactor(newfactor, newn);
-		console.log(factor.toString() + ",")
 		waysAcordingThefactor(newfactor, newn);
 	}
 
 }
 
+function howManyWays(n) {
+	const res = {};
 
-waysAcordingThefactor(5, 5);
-waysAcordingThefactor(4, 5);
-waysAcordingThefactor(3, 5);
-waysAcordingThefactor(2, 5);
-waysAcordingThefactor(1, 5);
+	for(let i = n; i >= 1; i--) {
+		console.log(i)
+		waysAcordingThefactor(i, n, res);
+	}
 
-console.log(res)
+	console.log(res);
+
+}
+
+
+howManyWays(6);
+
+
+
 
 
 
