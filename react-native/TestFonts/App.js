f49e260b4a0bab680830a29a8ebf234f0580432f
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-01-02 10:51:58 -0300

    #

--- react-native/TestFonts/App.js
+++ react-native/TestFonts/App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    "Josefin_Sans": require('./assets/fonts/Josefin_Sans/static/JosefinSans-Regular.ttf'),
+    "Audiowide": require('./assets/fonts/Audiowide/Audiowide-Regular.ttf')
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-      <StatusBar style="auto" />
-    </View>
+    fontsLoaded  
+    ? (
+      <View style={styles.container}>
+        <Text style={styles.textStile}>This is just a test</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+    : (
+      <AppLoading />
+    )
   );
 }
 
@@ -18,4 +32,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textStile: {
+    fontFamily: 'Audiowide', fontSize: 30
+  }
 });
