a2a7177c7010cd0a76f0dee392c89058952bac75
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-01-08 09:11:03 -0300

    #

--- algorithms/main.js
+++ algorithms/main.js
@@ -15,95 +15,60 @@ Array.prototype.equals = function(arr) {
 
 }
 
-function getSum(arr) {
-	/* give the result of elements array sum */
-	let sum = arr.reduce((acum, curr) => {
-		return acum + curr;
-	});	
-	return sum;
+function elementsSum(arr) {
+	let mySum = arr.reduce((acum, curr) => acum + curr);
+	return mySum
 }
 
-function complete(arr, n, factor) {	
-	/* complete whit factor until the sum to be equals n */
-	if(typeof arr != "undefined") {
-		let sum = getSum(arr);
+function complete(arr, n) {
+	console.log(`arr: ${arr}; n: ${n}`)
+
+	let sum = elementsSum(arr);
+	console.log(`sum: ${sum}`)
 	
-		while(getSum(arr) < n) {
-			arr.push(factor);
-		}
-	}
-}
+	arr.push(1)
 
-function howManyWays(size, n, factor) {
-	let arr = new Array(size);
-	arr.fill(1); 
-	let x = 0;
-	for(let i in arr) {
-		x += arr.reverse().pop();
-		if(x == factor) {
-			arr.push(factor);
-			return arr.reverse();
-		};
-	}	
-}
+	console.log(`arr: ${arr}, n: ${n}`)
 
-function main(n, factor) {
-	let ways = [];
-	let count = 0;
-	let size = n - (count * factor);	
-
-	while(size >= factor) {
-		let res = howManyWays(size, n, factor);
-		complete(res, n, factor);
-		if(typeof res != "undefined") ways.push(res.sort().reverse());
-		count++;
-		size = n - (count * factor);
-	}
-
-	return ways;
+	sum = elementsSum(arr);
+	console.log(`sum: ${sum}`)
 	
+	// console.log(arr);
 }
 
-function start(n) {
-	let factor = 2;
+function main(n) {
 	let ways = [];
 
-	if(n > 2) {
-		while(factor < n) {
-			let w = main(n, factor);
-			if (w.length > 0) ways.push(w);
-			factor++;
+	for(let i = 1; i <= n; i++) {
+		let way = [];
+		let result = parseInt(n / i);
+		let remain = n % i;	
+
+		if(i + remain == 5) {
+			way.push(i); 
+			if (remain !== 0) way.push(remain);
+			ways.push(way);
+		} else if(i + remain < 5) {
+			// console.log(i, remain)
+			let arr = []; arr.push(i);
+			if(remain !== 0) arr.push(remain);
+			complete(arr, n)
 		}
-	}	else if(n === 2) {
-		ways.push([2]);
+
+		if(i + result == 5) {
+			if(result !== remain) {
+				way.push(i); way.push(result);
+				ways.push(way);
+			}
+		} else if(i + result < 5) {
+			// console.log(i, result)
+			complete([i, result], n)
+		}
 	}
-	
-	return ways;
+
+	console.log(ways);
 
 }
 
-// console.log(start(2))
-
-function test(n) {
-	let res = start(n);
-
-	res.forEach(ways => {
-		ways.forEach(way => {
-			let keep = way.filter(e => e != 1);
-			let ones = way.filter(e => e == 1);
-			console.log(keep, ones);
-			console.log(start(ones.length)) 	
-			console.log("\n");		
-		})
-	})
-
-	console.log("\n");
-	return res;
-	/*
-	When you have more the algarism 1 more then one time and the factor 
-	is grather then two, you need look for more ways
-	*/
-}
-
-test(7)
+main(5);
 
