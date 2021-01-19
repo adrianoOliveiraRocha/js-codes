b12dc8e757e7894b192e215aaf3ccd6f8ce6eb34
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-01-19 14:44:48 -0300

    #

--- algorithms/test.js
+++ algorithms/test.js
@@ -1,20 +1,17 @@
 /*
-Given the value n, find, if any, 3 consecutive even numbers whose sum is equal to n.
+
+Exemplo 2
+
+O triplo de um número natural somado a 4 é igual ao quadrado de 5. Calcule-o:
+
+Resolução:
+
+3x + 4 = 5²
+3x = 25 – 4
+3x = 21
+x = 21/3
+x = 7
+
+O número procurado é igual a 7.
+
 */
-
-function resolve(n) {
-	
-	for (let i = 0; (i + (i + 1) + (i + 4)) < n; i++) {
-		if(i % 2 === 0) {
-			if(i + (i + 2) + (i + 4) === n) return [i, (i + 2), (i + 4)];
-		}
-	}
-	return false;
-}
-
-let response = resolve(18);
-response ? console.log(response) : console.log("Doesn't exists");
-
-
-
-
--- react-native/TestAudio/components/Player.js
+++ react-native/TestAudio/components/Player.js
@@ -74,5 +74,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }  
 });
