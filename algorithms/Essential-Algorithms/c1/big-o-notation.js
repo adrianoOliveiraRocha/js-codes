92a82ed12278dc6bd0d1481f4ff81267dd8bcf04
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-02-04 10:05:16 -0300

    #

--- algorithms/Essential-Algorithms/c1/big-o-notation.js
+++ algorithms/Essential-Algorithms/c1/big-o-notation.js
@@ -72,7 +72,40 @@ O(N) * O(N) == O(N**2)
 O(1) + O(1) = O(2)
 Then we have O(2 + N**2)
 Rule 5 lets you ignore factor 2 (it is constant) , so the runtime is O(N**2)
+Why?
+Test: N => 2 * N - multiplie for 2
+2 * (2 * N) = 8N**2 => increased by factor 4
+(2 * N)**2 = 4N**2 => increased by factor 4
 
+*/
+
+function dividingPoint(arr) {
+	/*
+	This algorithm picks the values at the beginning, end, and middle of the
+	array, compares them, and returns whichever item lies between the other two
+	*/
+	console.log(arr);
+	let n1 = arr[0]; // first
+	let n2 = arr[parseInt(arr.length / 2)]; /*midle*/ 
+	let n3 = arr[arr.length - 1]; // last
+		
+	if(n1 > n2 && n1 < n3) return n1;
+	if(n2 > n1 && n2 < n3) return n2;
+	
+	return n3;
+}
+
+let arr = [];
+for(let i = 0; i <= 100; i++) {
+	if(1 % 2 == 0) arr.push(i);
+	else arr.push(i * 7);
+}
+
+console.log(dividingPoint(arr));
+
+/*
+Because this algorithm performs only a few fixed steps, it has O(1) perfor-
+mance and its runtime is independent of the number of inputs N.
 */
 
 
--- algorithms/Essential-Algorithms/test.js
+++ algorithms/Essential-Algorithms/test.js
@@ -1,4 +1,31 @@
-function test(t) {
-	console.log(t);
+function dividingPoint(arr) {
+	/*
+	This algorithm picks the values at the beginning, end, and middle of the
+	array, compares them, and returns whichever item lies between the other two
+	*/
+	console.log(arr);
+	let n1 = arr[0]; // first
+	let n2 = arr[parseInt(arr.length / 2)]; /*midle*/ 
+	let n3 = arr[arr.length - 1]; // last
+		
+	if(n1 > n2 && n1 < n3) return n1;
+	if(n2 > n1 && n2 < n3) return n2;
+	
+	return n3;
 }
+
+let arr = [];
+for(let i = 0; i <= 100; i++) {
+	if(1 % 2 == 0) arr.push(i);
+	else arr.push(i * 7);
+}
+
+console.log(dividingPoint(arr));
+
+/*
+Because this algorithm performs only a few fixed steps, it has O(1) perfor-
+mance and its runtime is independent of the number of inputs N.
+*/
+
+
 
