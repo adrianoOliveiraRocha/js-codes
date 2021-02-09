4836fd30f69e483b205b21d7d98cf4245097712e
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-02-09 09:56:10 -0300

    #

--- react-native/WorkingInBackGround/App.js
+++ react-native/WorkingInBackGround/App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Tm from './components/Tm';
+import MyTask from './services/MyTask'
 
 export default function App() {
+  const registerMyTask = () => {
+    MyTask.register()
+      .then(() => console.log("task registred"))
+      .catch(error => console.error(error))
+  }
+
+  const unRegisterMyTask = () => {
+    MyTask.unregister()
+      .then(() => console.log("task unregistred"))
+      .catch(error => console.error(error))
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Trying working in background...</Text>
-      <Tm />
+      <Button 
+        title="Register"
+        onPress={() => registerMyTask()}
+      />
+      <Button 
+        title="Unregister"
+        onPress={() => unRegisterMyTask()}
+      />
       <StatusBar style="auto" />
     </View>
   );
