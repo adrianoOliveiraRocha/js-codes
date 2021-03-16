ff7074812396632e17f6c2f640caf03d5ee6aff6
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-03-16 10:54:21 -0300

    #

---
+++ algorithms/Essential-Algorithms/c2/generator.js
@@ -0,0 +1,16 @@
+// X n+1 = (A × X n + B) Mod M
+function Generator(seed) {
+  let a = 7, b = 5, m = 11; // constants
+  let x = (a * seed + b) % m; 
+  let result = [];
+  
+  do {
+    x = (a * x + b) % m;
+    result.push(x);    
+  } while (x != seed);
+
+  return result;
+  
+}
+
+console.log(Generator(8)); // until 10
--- algorithms/Essential-Algorithms/test.js
+++ algorithms/Essential-Algorithms/test.js
@@ -1,9 +1,24 @@
 'strict'
 
-const TwoSimultaneousDices = {
-  u1: [1, 2, 3, 4, 5, 6],
-  u2: [1, 2, 3, 4, 5, 6],
+/*
+In a ballot box there are 50 cards numbered from 1 to 50. One card is
+taken at random. Determine the likelihood that the number of the
+card:
+a) is a cousin;
+b) end with the digit 7.
+*/
 
-  
+function letsGo() {
+  let cousin = [], sevenDigit = [];
 
+  for(let i = 1; i <= 50; i++) {
+    if(i % 2 != 0) cousin.push(i); 
+    let t = JSON.stringify(i);
+    if(t.length === 1 & t[0] === '7') sevenDigit.push(i);
+    if(t.length === 2 & t[1] === '7') sevenDigit.push(i);
+  }
+
+  return [`${(cousin.length / 50) * 100}%; ${(sevenDigit.length / 50) * 100}%`];
 }
+
+console.log(letsGo());
