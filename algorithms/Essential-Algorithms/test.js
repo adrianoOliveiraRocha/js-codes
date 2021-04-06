c78d1c9065f3e129367465edf75e5191121aa01c
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-04-06 08:41:07 -0300

    #

--- algorithms/Essential-Algorithms/test.js
+++ algorithms/Essential-Algorithms/test.js
@@ -1,24 +1,11 @@
 'strict'
 
-/*
-In a ballot box there are 50 cards numbered from 1 to 50. One card is
-taken at random. Determine the likelihood that the number of the
-card:
-a) is a cousin;
-b) end with the digit 7.
-*/
-
-function letsGo() {
-  let cousin = [], sevenDigit = [];
-
-  for(let i = 1; i <= 50; i++) {
-    if(i % 2 != 0) cousin.push(i); 
-    let t = JSON.stringify(i);
-    if(t.length === 1 & t[0] === '7') sevenDigit.push(i);
-    if(t.length === 2 & t[1] === '7') sevenDigit.push(i);
+function raiseToPower(a, p) {
+  let r = 1;
+  for(let i = 1; i <= p; i++) {
+    r = r * a;
   }
-
-  return [`${(cousin.length / 50) * 100}%; ${(sevenDigit.length / 50) * 100}%`];
+  console.log(r);
 }
 
-console.log(letsGo());
+raiseToPower(2, 5);
