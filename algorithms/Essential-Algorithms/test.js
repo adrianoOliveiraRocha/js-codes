314886097ba6d0a7abeee8f71125d47d398345ae
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-04-08 07:47:38 -0300

    #

--- algorithms/Essential-Algorithms/test.js
+++ algorithms/Essential-Algorithms/test.js
@@ -1,11 +1,20 @@
 'strict'
 
+/*
+There’s a faster way to perform this kind of operation. This method
+is based on two key facts about exponentiation:
+■ A 2 × M = (A M ) 2
+■ A M+N = A M × A N
+*/
+
 function raiseToPower(a, p) {
   let r = 1;
-  for(let i = 1; i <= p; i++) {
-    r = r * a;
-  }
-  console.log(r);
+  if(p > 2) {
+    for(let i = 2; i < p; i=i+2) {
+      r = r * Math.pow(a, i);
+    }
+  }  
+  return r;
 }
 
-raiseToPower(2, 5);
+console.log(raiseToPower(7, 6));
