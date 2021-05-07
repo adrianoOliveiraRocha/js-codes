d92e9a29f9c271b3d57b62532b8e4f810f91086d
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-05-07 08:42:30 -0300

    #

--- JSDesignPattenrs/tests.js
+++ JSDesignPattenrs/tests.js
@@ -2,22 +2,7 @@
 
 // Object.create(prototype [, propertiesObject ] )
 
-function Person(name) {
-  this.name = name
-}
 
-var Woman = Object.create(Person)
-Woman.prototype.weithPuzzy = function(weigth) {
-  this.weigth = weigth
-}
-Woman.prototype.getWeigth = function() {
-  return this.weigth
-}
-console.log(Person)
-console.log(Woman)
-
-let w1 = new Woman()
-w1.weigth(50)
 
 
 
