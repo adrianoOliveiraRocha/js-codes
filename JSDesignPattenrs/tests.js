2009a77f367e57c5f59851c9b48ec13e7a3cf23e
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-05-07 09:35:21 -0300

    #

--- JSDesignPattenrs/tests.js
+++ JSDesignPattenrs/tests.js
@@ -1,10 +1,65 @@
 'strict'
 
-// Object.create(prototype [, propertiesObject ] )
+const KingJoffery = (function () {
 
+  function KingJoffery() {
 
+  }
 
+  KingJoffery.prototype.makeDecision = function() {
 
+  }
 
+  KingJoffery.prototype.marry = function() {
 
+  }
 
+  return KingJoffery
+
+})
+
+const LordTywin = (function() {
+  
+  function LordTywin() {
+
+  }
+
+  LordTywin.prototype.makeDecision = function() {
+
+  }
+
+  return LordTywin
+
+})
+
+const LannisterFactory = (function() {
+  
+  function LannisterFactory() {
+
+  }
+
+  LannisterFactory.prototype.getKing = function() {
+    return new KingJoffery()
+  }
+
+  LannisterFactory.prototype.getHandOfTheKing = function() {
+    return new LordTywin()
+  }
+
+  return LannisterFactory
+
+})
+
+const TargaryenFactory = (function() {
+  function TargaryenFactory() {
+
+  }
+
+  TargaryenFactory.prototype.getKing = function() {
+
+  }
+
+  TargaryenFactory.prototype.getHandOfTheKing = function() {
+    return new LordConnington
+  }
+})
